e06f65615e3bc418c186fb96fa68e1bef38a6dbe
xiacl33 <1324952217@qq.com>
2026-03-20 10:46:48 +0800

    create_sql_query_chain的使用：将自然语言查询转换为SQL查询

--- createSqlDemo.py
+++ createSqlDemo.py
@@ -1,4 +1,7 @@
+from langchain_classic.chains.sql_database.query import create_sql_query_chain
 from langchain_community.utilities import SQLDatabase
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama import OllamaLLM
 
 db_user = "root"
 db_password = "root"
@@ -12,6 +15,29 @@ print("哪种数据库：", db.dialect)
 print("数据库表：", db.get_usable_table_names())
 
 # 执行SQL查询
-query = "SELECT * FROM user LIMIT 5;"
+query = "SELECT count(*) FROM user;"
 print("查询结果：", db.run(query))
+
+
+# create_sql_query_chain的使用：将自然语言查询转换为SQL查询
+# 加载模型
+llm = OllamaLLM(
+    model="qwen2.5:3b",  # 换小模型
+    temperature=0.7,
+    num_ctx=1024,  # 上下文长度
+    top_p=0.9,
+)
+
+# 创建SQL查询链
+sql_query_chain = create_sql_query_chain(   
+    llm=llm,
+    db=db,
+    )       
+# 执行SQL查询链
+result = sql_query_chain.invoke({"question": "user表中有多少条记录。"})
+print("SQL查询链结果：", result)
+
+
+
+
 
